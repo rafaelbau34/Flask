d8a0fd70abf57d7f982d31776b7b43e287478ebc
rafael <rafaelbaugar@gmail.com>
2025-01-23 02:17:38 -0600

    avanzandole a la tarea

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,21 @@
-from flask import Blueprint, jsonify, render_template
+from flask import Flask, Blueprint, jsonify, render_template, flash, request
 
 # Crea un blueprint para organizar las rutas
+
 main = Blueprint('main', __name__)
+
 
 # Ruta para la página web
 @main.route('/')
 def home():
+    flash('¿Cual es tu nombre?')
     return render_template('index.html')
 
-# Ruta para la API     
-@main.route('/api/greet', methods=['GET'])
+@main.route('/greet', methods=['POST', 'GET'])
 def greet():
-    return jsonify({"message": "Hello, welcome to my API!"})
+   
+    flash('Hola ' + str(request.form['name_input']) + ', nos da gusto verte!')
+    return render_template('index.html')
  
 @main.route("/api/unid", methods= ['GET'])
 def unid():
